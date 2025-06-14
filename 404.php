2fb50b383ee99f8407112fcb6b7fdf7af7be4a7f
Jorge Diz Pico <xurxo@xurxodiz.eu>
2025-06-14 15:27:33 +0200

    Changes to 404 page

--- 404.php
+++ 404.php
@@ -4,6 +4,9 @@
 			<div class='post-header'>
 				<h1 class='post-title'><?php _e( '404: Non encontrado', 'author' ); ?></h1>
 			</div>
+			<img class="404-untuned-screen"
+			     src="<?php echo trailingslashit(get_template_directory_uri()) . 'assets/images/404.png'; ?>"
+				 width="600px" height="450px" />
 			<div class="post-content">
 				<?php _e( 'Neste enderezo non hai nada. Proba a buscar o que querías na seguinte caixa.', 'author' ); ?>
 			</div>
